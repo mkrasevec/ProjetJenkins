2fee1fb66900e859e25f115ab4fb8fd6bbf027e3
Mickaël Krasevec de Raucourt <mickael.krasevec@gmail.com>
2021-09-03 16:19:08 +0200

    modif app.py pour test unitaire

--- src/app.py
+++ src/app.py
@@ -4,6 +4,15 @@ def mettre_majuscule(string):
     cap_string= string.capitalize()
     return cap_string
 
-# string=input("Veuillez écrire votre phrase en minuscule")
-# string="la première lettre de cette phrase n'est plus une minuscule"
-# fonction(string)
+### string=input("Veuillez écrire votre phrase en minuscule")
+## string="la première lettre de cette phrase n'est plus une minuscule"
+## fonction(string)
+
+def capital_case(v):
+    return v.capitalize()
+
+def test_mettre_majuscule():  
+    resultat= capital_case("adrien")
+    assert resultat == 'Adrien'
+
+    #lancer pytest <chemin>, il va lancer le test et reconnaitre la fonction test grâce à la partie du nom de la fonction "test_"
